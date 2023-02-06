26afbe1c1ebd2ece861495ea4b1b8598c903f434
Juan José Revelo <revelojuan88@gmail.com>
2023-02-06 14:51:00 -0500

    Cambios

--- operaciones/urls.py
+++ operaciones/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from operaciones import views
 
 urlpatterns = [
-    path('/sumar/<int:op1>/<int:op2>', views.operar),
-    path('/restar/<int:op1>/<int:op2>', views.operar),
-    path('/multiplicar/<int:op1>/<int:op2>', views.operar),
-    path('/dividir/<int:op1>/<int:op2>', views.operar)
+    path('/sumar/<op1>/<op2>', views.sumar),
+    path('/restar/<op1>/<op2>', views.restar),
+    path('/multiplicar/<op1>/<op2>', views.multiplicar),
+    path('/dividir/<op1>/<op2>', views.dividir)
 ]
